c4e049357dc053a4698b11cc90accc4e50e67101
Marta SEstelo <sestelom@gmail.com>
2014-10-27 16:30:15 +0000

    solution to par(mfrow=c(1,2))

--- NPRegfast/R/plot.frfast.R
+++ NPRegfast/R/plot.frfast.R
@@ -7,16 +7,16 @@ plot.frfast<-function (model, fac = NULL, der = NULL, points = TRUE, xlab = "x",
     fi = length(fac)
     co = length(der)
     facini=fac
-if(fi==0&nf>1|fi>1|co==0&nf>1|co>=0){
-    if (fi == 0) fi = nf
-    if (co == 0) co = 3
+#if(fi==0&nf>1|fi>1|co==0&nf>1|co>=0){
+    if (fi == 0) fi = nf # for each of the factor's level
+    if (co == 0) co = 1 #only show estimation
     jnf = c()
  
-    par(mfrow = c(fi, co))
+    if(fi>0|co>0){par(mfrow = c(fi, co))}
     if (length(fac) == 0){jnf = c(1:nf);fac = model$etiquetas
     }else{for (i in 1:length(fac)) {jnf[i] = which(model$etiquetas == fac[i])}}
     
-    if (length(der) == 0) {jder = c(1:3)}
+    if (length(der) == 0) {jder = c(1)} #only show estimation
     else {jder = der + 1}
     
     for (j in jnf) {
@@ -29,8 +29,8 @@ if(fi==0&nf>1|fi>1|co==0&nf>1|co>=0){
                 
             title=main
              
-            if(length(fac)!=0&is.null(main) ) title = paste("Level", model$etiquetas[jnf])
-            if(length(facini)==0&is.null(main)){title=" "}
+            if(length(fac)!=1&is.null(main) ) title = paste("Level", model$etiquetas[j])
+           # if(length(facini)==0&is.null(main)){title=" "}
            # if(is.null(ylim)) ylim=ylim2
             plot(model$x, model$p[, der = i, fac = j], type = type, 
                 xlab = xlab, ylab = ylab2, col = col, main = title, ylim = ylim)
@@ -41,28 +41,34 @@ if(fi==0&nf>1|fi>1|co==0&nf>1|co>=0){
             lines(model$x, model$pl[, der = i, fac = j], lty = 2, col = ICcol, type = ICtype)
             lines(model$x, model$pu[, der = i, fac = j], lty = 2, col = ICcol, type = ICtype)
             if (i == 3) abline(h = 0, col = 2)}}
-}else{	 
-    	der=der+1
-    	jnf = c()
-    	title=main 
-    	if(length(fac)!=0){ for (i in 1:length(fac)) {jnf[i] = which(model$etiquetas == fac[i])}}else{jnf=1}
-    	
-       if(length(fac)==0&is.null(main)){title=""}
-       if(length(fac)!=0&is.null(main) ) title = paste("Level", model$etiquetas[jnf])  
-    	
-    	if(der==1) ylab2=ylab
-    	if(der==2) ylab2="first derivative"
-    	if(der==3) ylab2="second derivative"
-    	if((points == TRUE) & (der == 1)) {
-    		ylim2=c(min(model$ydata[model$fmod==jnf],na.rm=T),max(model$ydata[model$fmod==jnf],na.rm=T))
-    	}else{ylim2=c(min(model$p[,der,jnf],na.rm=T),max(model$p[,der,jnf],na.rm=T))}
-           if(is.null(ylim)) ylim=ylim2
-    	plot(model$x, model$p[, der, jnf], type = type, xlab = xlab, ylab = ylab2, col = col, main = title,
-    	     ylim = ylim)
-            if ((points == TRUE) & (der == 1)) {
-                points(model$xdata[model$fmod==jnf],model$ydata[model$fmod==jnf], col = "grey80", cex = 0.6)
-                lines(model$x,model$p[,der,jnf],type=type,xlab = xlab, ylab = ylab2, col = col, main = title)}
-       lines(model$x, model$pl[, der , jnf], lty = 2, col = ICcol, type = ICtype)
-       lines(model$x, model$pu[, der , jnf], lty = 2, col = ICcol, type = ICtype)
-             if (der == 3) abline(h = 0, col = 2)
-}}
+#}
+
+}
+
+
+
+# }else{	 
+#     	der=der+1
+#     	jnf = c()
+#     	title=main 
+#     	if(length(fac)!=0){ for (i in 1:length(fac)) {jnf[i] = which(model$etiquetas == fac[i])}}else{jnf=1}
+#     	
+#        if(length(fac)==0&is.null(main)){title=""}
+#        if(length(fac)!=0&is.null(main) ) title = paste("Level", model$etiquetas[jnf])  
+#     	
+#     	if(der==1) ylab2=ylab
+#     	if(der==2) ylab2="first derivative"
+#     	if(der==3) ylab2="second derivative"
+#     	if((points == TRUE) & (der == 1)) {
+#     		ylim2=c(min(model$ydata[model$fmod==jnf],na.rm=T),max(model$ydata[model$fmod==jnf],na.rm=T))
+#     	}else{ylim2=c(min(model$p[,der,jnf],na.rm=T),max(model$p[,der,jnf],na.rm=T))}
+#            if(is.null(ylim)) ylim=ylim2
+#     	plot(model$x, model$p[, der, jnf], type = type, xlab = xlab, ylab = ylab2, col = col, main = title,
+#     	     ylim = ylim)
+#             if ((points == TRUE) & (der == 1)) {
+#                 points(model$xdata[model$fmod==jnf],model$ydata[model$fmod==jnf], col = "grey80", cex = 0.6)
+#                 lines(model$x,model$p[,der,jnf],type=type,xlab = xlab, ylab = ylab2, col = col, main = title)}
+#        lines(model$x, model$pl[, der , jnf], lty = 2, col = ICcol, type = ICtype)
+#        lines(model$x, model$pu[, der , jnf], lty = 2, col = ICcol, type = ICtype)
+#              if (der == 3) abline(h = 0, col = 2)
+# }}
