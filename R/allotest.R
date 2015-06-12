805a426c93c6f7e09cea198c9ca052b9dfd82ce2
Marta Sestelo <sestelo@uvigo.es>
2015-06-12 13:11:32 +0200

    indent code and <-

--- R/allotest.R
+++ R/allotest.R
@@ -37,77 +37,82 @@
 #'@examples
 #' library(NPRegfast)
 #' data(barnacle)
-#' allotest(DW ~ RC, data = barnacle, nboot = 1000)
+#' allotest(DW ~ RC, data = barnacle)
 #' allotest(DW ~ RC : F, data = barnacle)
 
 
 
-#li### opcion cambiar bootstrap y argumentos... ver
-allotest <- function(formula, data = data, nboot = 100, kbin = 200, seed = NULL){
-
-	ffr <- interpret.frfastformula(formula, method = "frfast")
-	varnames <- ffr$II[2,]
-	aux<-unlist(strsplit(varnames,split=":"))
-	varnames<-aux[1]
-	namef<-aux[2]
-	if(length(aux)==1){f=NULL}else{f<-data[,namef]}
-	newdata <- data
-	data <- na.omit(data[,c(ffr$response, varnames)])
-	newdata <- na.omit(newdata[,varnames])
-	n=nrow(data)
-	
-  if(is.null(seed)){
-    set.seed(NULL)
-    seed <-.Random.seed[3]
+allotest <- function(formula, data = data, nboot = 100, kbin = 200, 
+                     seed = NULL) {
+  
+  ffr <- interpret.frfastformula(formula, method = "frfast")
+  varnames <- ffr$II[2, ]
+  aux <- unlist(strsplit(varnames, split = ":"))
+  varnames <- aux[1]
+  namef <- aux[2]
+  if (length(aux) == 1) {
+    f <- NULL
+  } else {
+    f <- data[, namef]
   }
-	if(is.null(f)) f <- rep(1.0,n)
-	etiquetas<-unique(f)
- 	nf<-length(etiquetas)
-		
-				
-		res=list()
-		for(i in etiquetas){
-			yy=data[,1][f==i]
-			xx=data[,2][f==i]
-      n=length(xx)
-			w=rep(1,n)
-			fit<-.Fortran("test_allo",
-				x=as.double(xx),
-				y=as.double(yy),
-				w=as.double(w),
-				n=as.integer(n),
-				kbin=as.integer(kbin),
-				nboot=as.integer(nboot),
-        seed=as.integer(seed),
-				T=as.double(-1.0),
-				pvalue = as.double(-1.0)
-				)	
-				
-			res[[i]]<- list(statistic=fit$T, 
-			pvalue=fit$pvalue)	
-		}
-		
-		
-		est=c()
-		p=c()
-		for(i in 1:length(res)){
-			est[i]=res[[i]]$statistic
-			p[i]=res[[i]]$pvalue	
-		}
-			
-	kk=cbind(est,p)
-	result<-matrix(kk,ncol=2,nrow=length(res))
-	colnames(result)=c("Statistic","pvalue")
-		
-		
-		#factores=paste("Factor",1:length(res))
-		factores=paste("Level",etiquetas[1:length(etiquetas)])
-		
-			
-		for(i in 1:length(res)){
-			rownames(result)=c(factores)
-		}
-			
-	return(result)
-		
-}
+  newdata <- data
+  data <- na.omit(data[, c(ffr$response, varnames)])
+  newdata <- na.omit(newdata[, varnames])
+  n <- nrow(data)
+  
+  if (is.null(seed)) {
+    set.seed(NULL)
+    seed <- .Random.seed[3]
+  }
+  if (is.null(f)) 
+    f <- rep(1, n)
+  etiquetas <- unique(f)
+  nf <- length(etiquetas)
+  
+  
+  res <- list()
+  
+  for (i in etiquetas) {
+    yy <- data[, 1][f == i]
+    xx <- data[, 2][f == i]
+    n <- length(xx)
+    w <- rep(1, n)
+    fit <- .Fortran("test_allo", 
+                    x = as.double(xx), 
+                    y = as.double(yy), 
+                    w = as.double(w), 
+                    n = as.integer(n), 
+                    kbin = as.integer(kbin), 
+                    nboot = as.integer(nboot), 
+                    seed = as.integer(seed), 
+                    T = as.double(-1), 
+                    pvalue = as.double(-1)
+                    )
+    
+    res[[i]] <- list(statistic = fit$T, pvalue = fit$pvalue)
+  }
+  
+  
+  est <- c()
+  p <- c()
+  for (i in 1:length(res)) {
+    est[i] <- res[[i]]$statistic
+    p[i] <- res[[i]]$pvalue
+  }
+  
+  kk <- cbind(est, p)
+  result <- matrix(kk, ncol = 2, nrow = length(res))
+  colnames(result) <- c("Statistic", "pvalue")
+  
+  
+  # factores=paste('Factor',1:length(res))
+  factores <- paste("Level", etiquetas[1:length(etiquetas)])
+  
+  
+  for (i in 1:length(res)) {
+    rownames(result) <- c(factores)
+  }
+  
+  return(result)
+  
+} 
