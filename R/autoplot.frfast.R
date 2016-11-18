972970d6a036ad99f1ee176c573a81f4fab29bc0
Marta Sestelo <sestelo@uvigo.es>
2016-11-18 18:01:25 +0100

    new pre-release

--- R/autoplot.frfast.R
+++ R/autoplot.frfast.R
@@ -73,26 +73,29 @@ ggplot2::autoplot
 #' # Nonparametric regression with interactions
 #' fit2 <- frfast(DW ~ RC : F, data = barnacle, nboot = 100) 
 #' autoplot(fit2, fac = "barca")
-#' plot(fit2, der = 1, fac = "lens")
+#' # autoplot(fit2, der = 1, fac = "lens")
 #' 
 #' # Visualization of the differences between two factor's levels
 #' autoplot(fit2, fac = "barca", diffwith = "lens")
-#' autoplot(fit2, der = 1, fac = "barca", diffwith = "lens")
+#' # autoplot(fit2, der = 1, fac = "barca", diffwith = "lens")
 #' 
 #' 
-# ' #Plotting in the same graphics device
-# '
-# ' # if (requireNamespace("gridExtra", quietly = TRUE)) {
-# '
-# ' # # For plotting two derivatives in the same graphic windows
-# ' # ders <- lapply(0:1, function(x) autoplot(fit, der = x))
-# ' # gridExtra::grid.arrange(grobs = ders, ncol = 2, nrow = 1)
-# '
-# ' # # For plotting two levels in the same graphic windows
-# ' # facs <- lapply(c("barca", "lens"), function(x) autoplot(fit2, der = 0, fac = x))
-# ' # gridExtra::grid.arrange(grobs = facs, ncol = 2, nrow = 1)
-# '
-# ' # }
+#' #Plotting in the same graphics device
+#' \dontrun{
+#' 
+#' if (requireNamespace("gridExtra", quietly = TRUE)) {
+#' 
+#' # For plotting two derivatives in the same graphic windows
+#' ders <- lapply(0:1, function(x) autoplot(fit, der = x))
+#' gridExtra::grid.arrange(grobs = ders, ncol = 2, nrow = 1)
+#'
+#' # For plotting two levels in the same graphic windows
+#' facs <- lapply(c("barca", "lens"), function(x) autoplot(fit2, der = 0, fac = x))
+#' gridExtra::grid.arrange(grobs = facs, ncol = 2, nrow = 1)
+#' 
+#' }
+#' 
+#' }
 #' 
 #' @importFrom ggplot2 autoplot geom_point aes_string ylab
 #' @importFrom ggplot2 geom_hline geom_ribbon geom_line 
